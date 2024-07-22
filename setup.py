fe4553221a9e8abf726608bca24e9e51c8d10aa2
yuta6 <46110512+yuta6@users.noreply.github.com>
2024-07-22 14:11:15 +0900

    add : 依存関係をdev用とエンドユーザー用に書き分けた

--- setup.py
+++ setup.py
@@ -1,23 +1,36 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 
 def parse_requirements(filename):
     with open(filename) as f:
         return f.read().splitlines()
+
 
 setup(
     name="AtCoderStudyBooster",
     version="0.1",
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'atcdr=atcdr.main:main',
+        "console_scripts": [
+            "atcdr=atcdr.main:main",
         ],
     },
-    install_requires=parse_requirements('requirements.txt'),
+    install_requires=parse_requirements("requirements.txt"),
+    extras_require={
+        "dev": [
+            "black",
+            "isort",
+            "flake8",
+            "mypy",
+            "pre-commit",
+            "pytest",
+            # 他の開発者向けの依存関係
+        ],
+    },
     include_package_data=True,
     description="A tool to download and manage AtCoder problems.",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="yuta6",
     url="https://github.com/yuta6/AtCoderStudyBooster",
 )
