937617a306f79eb2bd7f1dee294efbcf64dff2ce
yuta6 <46110512+yuta6@users.noreply.github.com>
2024-07-27 14:53:19 +0900

    add : 実装したいことの計画を立てた

--- atcdr/generate.py
+++ atcdr/generate.py
@@ -1,10 +1,30 @@
-from dataclasses import dataclass
+class ProblemStruct:
+    def __init__(self) -> None:
+        self.problem_block = None
+        self.condition_block = None
+        self.test_block = None
+
+    def parse_html(self) -> None:
+        pass
 
 
-@dataclass
-class Option:
+# ChatGPTクラスを利用して競技プログラムの問題を解く
+# generateのオプション
+# atcdr generate
+def generate() -> None:
+
+    # カレントディレクトリの問題文のHTMLファイルを読み込む
+
+    # 読み込んだHTMLファイルをparseして3つの情報を取得する
+    # 問題文, 変数の制約, テストケース
+
+    # 適切なプロンプトを作成してGPTに与える
+
+    # GPTからの返答に対して解答ファイルを作成する
+
+    # 解答ファイルをテストする
+
+    # テスト結果をパスした場合はパスしたファイルを保存する
+    # しなかった場合は, その結果をプロンプトに与えて,再度ファイルをGPTに解答ファイルをつくってもらう
+
     pass
-
-
-def generate(problem_paragrah: str, option: Option) -> str:
-    return problem_paragrah + str(Option)
