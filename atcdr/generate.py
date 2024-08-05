82450e7a51bedadcc29ec5edf4807ce4b0349500
yuta6 <46110512+yuta6@users.noreply.github.com>
2024-08-05 11:55:59 +0900

    fix : markdownファイルを解析する処理を他の場所に書く

--- atcdr/generate.py
+++ atcdr/generate.py
@@ -1,58 +1,60 @@
-import re
-
-from bs4 import BeautifulSoup as bs
-from markdownify import MarkdownConverter
-
+from atcdr.util.filename import FileExtension, Filename, execute_files
 from atcdr.util.gpt import ChatGPT, set_api_key
+from atcdr.util.problem import make_problem_markdown
 
 
-class CustomMarkdownConverter(MarkdownConverter):
+def solve_problem(file: Filename, lang: str) -> None:
 
-    def convert_var(self, el, text, convert_as_inline):
-        var_text = el.text.strip()
-        return f"\\({var_text}\\)"
-
-    def convert_pre(self, el, text, convert_as_inline):
-        pre_text = el.text.strip()
-        return f"```\n{pre_text}\n```"
+    with open(file, "r") as f:
+        html_content = f.read()
+    html_content
 
 
-def custom_markdownify(html, **options):
-    return CustomMarkdownConverter(**options).convert(html)
+def generate_code(file: Filename, lang: str) -> None:
 
-
-def remove_unnecessary_newlines(md_text):
-    md_text = re.sub(r"\n\s*\n\s*\n+", "\n\n", md_text)
-    md_text = md_text.strip()
-    return md_text
-
-
-def make_problem_markdown(html_content: str, lang: str) -> str:
-    soup = bs(html_content, "html.parser")
-    task_statement = soup.find("div", {"id": "task-statement"})
-    if lang == "ja":
-        lang_class = "lang-ja"
-    elif lang == "en":
-        lang_class = "lang-en"
-    else:
-        pass
-    span = task_statement.find("span", {"class": lang_class})
-    return str(span)
-
-
-# ChatGPTクラスを利用して競技プログラムの問題を解く
-# generateのオプション --with-no-test
-# atcdr generate
-def generate(*args: str, without_test: bool = False) -> None:
-
+    with open(file, "r") as f:
+        html_content = f.read()
     if set_api_key():
         return
     ChatGPT(
         system_prompt="You are a genius programmer. Your job is to generate the correct code for the problem.",
     )
+    html_content
 
-    # カレントディレクトリの問題文のHTMLファイルを読み込む
-    # open.pyと似たロジックなのでコンポーネント化したい。
+
+def generate_template(file: Filename, lang: str) -> None:
+
+    with open(file, "r") as f:
+        html_content = f.read()
+    md = make_problem_markdown(html_content, "en")
+    md
+
+
+def generate(
+    *source: str,
+    lang: str = "python",
+    without_test: bool = False,
+    template: bool = False
+) -> None:
+
+    if template:
+        execute_files(
+            *source,
+            func=lambda file: generate_template(file, lang),
+            target_filetypes=[FileExtension.HTML]
+        )
+    elif without_test:
+        execute_files(
+            *source,
+            func=lambda file: generate_code(file, lang),
+            target_filetypes=[FileExtension.HTML]
+        )
+    else:
+        execute_files(
+            *source,
+            func=lambda file: solve_problem(file, lang),
+            target_filetypes=[FileExtension.HTML]
+        )
 
     # 適切なプロンプトを作成してGPTに与える
 
--- atcdr/util/problem.py
+++ atcdr/util/problem.py
@@ -1,7 +1,9 @@
+import re
 from enum import Enum
 from typing import Optional
 
 from bs4 import BeautifulSoup as bs
+from markdownify import MarkdownConverter
 
 
 # TODO : そのうちgenerate.pyやtest.py, open.pyのHTMLのparse処理を全部まとめる
@@ -31,3 +33,44 @@ class ProblemStruct:
         # 指定したdiv内の1つ目のclass="part"のdivをio_partに格納する
         self.io_part = io_parts[0]
         # 指定したdiv内の2つ目以降のclass="part"のdivをtest_partのリストに格納する
+
+
+class CustomMarkdownConverter(MarkdownConverter):
+
+    def convert_var(self, el, text, convert_as_inline):
+        var_text = el.text.strip()
+        return f"\\({var_text}\\)"
+
+    def convert_pre(self, el, text, convert_as_inline):
+        pre_text = el.text.strip()
+        return f"```\n{pre_text}\n```"
+
+
+def custom_markdownify(html, **options):
+    return CustomMarkdownConverter(**options).convert(html)
+
+
+def remove_unnecessary_emptylines(md_text):
+    md_text = re.sub(r"\n\s*\n\s*\n+", "\n\n", md_text)
+    md_text = md_text.strip()
+    return md_text
+
+
+def abstract_problem_part(html_content: str, lang: str) -> str:
+    soup = bs(html_content, "html.parser")
+    task_statement = soup.find("div", {"id": "task-statement"})
+    if lang == "ja":
+        lang_class = "lang-ja"
+    elif lang == "en":
+        lang_class = "lang-en"
+    else:
+        pass
+    span = task_statement.find("span", {"class": lang_class})
+    return str(span)
+
+
+def make_problem_markdown(html_content: str, lang: str) -> str:
+    problem_part = abstract_problem_part(html_content, lang)
+    problem_md = custom_markdownify(problem_part)
+    problem_md = remove_unnecessary_emptylines(problem_md)
+    return problem_md
