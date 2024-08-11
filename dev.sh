a0195a97cd8ae6c8558bf4df3a4bcabbfe872571
yuta6 <46110512+yuta6@users.noreply.github.com>
2024-08-11 23:01:34 +0900

    fix : 環境構築コマンドのシェルスクリプトを修正

--- dev.sh
+++ dev.sh
@@ -1,24 +1,14 @@
-# プロジェクトのルートディレクトリに移動
-cd "$(dirname "$0")"
-
 # 仮想環境の作成
-if [ ! -d "venv" ]; then
-    python3 -m venv venv
-    echo "仮想環境 'venv' が作成されました。"
-else
-    echo "仮想環境 'venv' は既に存在します。"
-fi
+python3 -m venv venv
 
 # 仮想環境をアクティブ化
 source venv/bin/activate
 
 # 必要なパッケージをインストール
-pip install --upgrade pip
-pip install -e .[dev]
+python -m pip install -e .\[dev\]
 
 # pre-commitのインストールと設定
+pre-commit
 pre-commit clean
 pre-commit install
 pre-commit autoupdate
-
-echo "開発環境のセットアップが完了しました。仮想環境が有効になっています。"
