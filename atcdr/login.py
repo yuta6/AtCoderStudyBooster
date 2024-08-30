415623935d6413de6d80694f578d62c0c21e7ce5
yuta6 <46110512+yuta6@users.noreply.github.com>
2024-08-31 01:09:54 +0900

    feat : loginコマンドを実装

--- atcdr/login.py
+++ atcdr/login.py
@@ -1,93 +1,42 @@
-from urllib.parse import unquote
-
 import requests
 from bs4 import BeautifulSoup as bs
-from rich import print
-from rich.align import Align
-from rich.panel import Panel
-from rich.syntax import Syntax
-from rich.table import Table
+from rich.console import Console
+from rich.prompt import Prompt
+
+from atcdr.util.session import load_session, save_session
 
 
-def print_response(response: requests.Response) -> None:
-    # レスポンス情報をテーブル形式で表示
-    info_table = Table(title='レスポンス情報')
-    info_table.add_column('項目', justify='left', style='cyan', no_wrap=True)
-    info_table.add_column('内容', justify='left', style='magenta')
-    info_table.add_row('URL', response.url)
-    info_table.add_row('ステータスコード', str(response.status_code))
-    info_table.add_row('理由', response.reason)
-    info_table = Align.center(info_table)
-
-    # ヘッダー情報をテーブル形式で表示
-    header_table = Table(title='レスポンスヘッダー')
-    header_table.add_column('キー', style='cyan', no_wrap=True)
-    header_table.add_column('値', style='magenta', overflow='fold')
-    for key, value in response.headers.items():
-        value = unquote(value)
-        header_table.add_row(key, value)
-    header_table = Align.center(header_table)
-
-    # レスポンスボディの表示
-    content_type = response.headers.get('Content-Type', '').lower()
-    if 'application/json' in content_type:
-        # JSONの整形表示
-        try:
-            body = Syntax(response.json(), 'json', theme='monokai', line_numbers=True)
-        except Exception:
-            pass
-    else:
-        # HTMLやその他のコンテンツの整形表示
-        body = (
-            Syntax(
-                response.text,
-                'html' if 'html' in content_type else 'text',
-                theme='monokai',
-                line_numbers=True,
-                line_range=(0, 24),
-            )
-            if response.text
-            else None
-        )
-    body_panel = Panel(body, title='レスポンスボディ') if body else None
-
-    print(info_table)
-    print(header_table)
-    if body:
-        print(body_panel)
-
-
-def login() -> requests.Session:
-    session = requests.Session()
-
+def login() -> None:
     ATCODER_LOGIN_URL = 'https://atcoder.jp/login'
     ATCODER_HOME_URL = 'https://atcoder.jp/home'
 
+    console = Console()
+    session = load_session()
+    if session:
+        console.print('[green][+][/] すでにログインしています.  ')
+        return
+
+    username = Prompt.ask('[cyan]ユーザー名を入力してください[/]', console=console)
+    password = Prompt.ask('[cyan]パスワードを入力してください[/]', console=console)
+
+    session = requests.Session()
     response = session.get(ATCODER_LOGIN_URL)
 
     soup = bs(response.text, 'html.parser')
     csrf_token = soup.find('input', {'name': 'csrf_token'})['value']
 
     login_data = {
-        'username': 'YutaY9',  # ユーザー名を入力
-        'password': '',  # パスワードを入力
+        'username': username,  # ユーザー名を入力
+        'password': password,  # パスワードを入力
         'csrf_token': csrf_token,  # 取得したCSRFトークンを使用
     }
 
-    login_response = session.post(ATCODER_LOGIN_URL, data=login_data)
-    if login_response.history:
-        for resp in login_response.history:
-            print_response(resp)
-
-    print_response(login_response)
+    with console.status('ログイン中'):
+        login_response = session.post(ATCODER_LOGIN_URL, data=login_data)
 
     # リクエスト後の最終URLを表示
     if login_response.url == ATCODER_HOME_URL and login_response.status_code == 200:
-        print('[green][+][/] ログインに成功しました.  ')
-        return session
+        console.print('[green][+][/] ログインに成功しました.  ')
+        save_session(session)
     else:
-        print('[green][-][/] ログインに失敗しました.  ')
-        return session
-
-
-login()
+        console.print('[green][-][/] ログインに失敗しました.  ')
---
+++ atcdr/util/session.py
@@ -0,0 +1,111 @@
+import json
+import os
+from typing import Optional
+from urllib.parse import unquote
+
+import requests
+from rich import print
+from rich.align import Align
+from rich.panel import Panel
+from rich.syntax import Syntax
+from rich.table import Table
+
+
+def print_rich_response(response: requests.Response) -> None:
+    # レスポンス情報をテーブル形式で表示
+    info_table = Table(title='レスポンス情報')
+    info_table.add_column('項目', justify='left', style='cyan', no_wrap=True)
+    info_table.add_column('内容', justify='left', style='magenta')
+    info_table.add_row('URL', response.url)
+    info_table.add_row('ステータスコード', str(response.status_code))
+    info_table.add_row('理由', response.reason)
+    info_table = Align.center(info_table)
+
+    # ヘッダー情報をテーブル形式で表示
+    header_table = Table(title='レスポンスヘッダー')
+    header_table.add_column('キー', style='cyan', no_wrap=True)
+    header_table.add_column('値', style='magenta', overflow='fold')
+    for key, value in response.headers.items():
+        value = unquote(value)
+        header_table.add_row(key, value)
+    header_table = Align.center(header_table)
+
+    # レスポンスボディの表示
+    content_type = response.headers.get('Content-Type', '').lower()
+    if 'application/json' in content_type:
+        # JSONの整形表示
+        try:
+            body = Syntax(response.json(), 'json', theme='monokai', line_numbers=True)
+        except Exception:
+            pass
+    else:
+        # HTMLやその他のコンテンツの整形表示
+        body = (
+            Syntax(
+                response.text,
+                'html' if 'html' in content_type else 'text',
+                theme='monokai',
+                line_numbers=True,
+                line_range=(0, 24),
+            )
+            if response.text
+            else None
+        )
+    body_panel = Panel(body, title='レスポンスボディ') if body else None
+
+    print(info_table)
+    print(header_table)
+    if body:
+        print(body_panel)
+
+
+def load_session() -> Optional[requests.Session]:
+    cookie_file = os.path.join('~', 'cache', 'atcder', 'session.json')
+    if not os.path.exists(cookie_file):
+        return None
+    else:
+        with open(cookie_file) as file:
+            session = requests.Session()
+            session.cookies.update(json.load(file))
+        if validate_session(session):
+            return requests.Session()
+        else:
+            return None
+
+
+def save_session(session: requests.Session) -> None:
+    if validate_session(session):
+        cookie_file = os.path.join('~', 'cache', 'atcder', 'session.json')
+        os.makedirs(os.path.dirname(cookie_file), exist_ok=True)
+        with open(cookie_file, 'w') as file:
+            json.dump(session.cookies.get_dict(), file)
+    else:
+        pass
+
+
+def validate_session(session: requests.Session) -> bool:
+    ATCODER_SETTINGS_URL = 'https://atcoder.jp/settings'
+    try:
+        response = session.get(
+            ATCODER_SETTINGS_URL, allow_redirects=False
+        )  # リダイレクトを追跡しない
+        if response.status_code == 200:
+            return True
+        elif response.status_code in (301, 302) and 'Location' in response.headers:
+            redirect_url = response.headers['Location']
+            if 'login' in redirect_url:
+                print('[red][-][/] セッションが無効です ログインしてください.')
+                return False
+        print(
+            f'[red][-][/] セッションが無効です ステータスコード: {response.status_code}'
+        )
+        return False
+    except requests.RequestException as e:
+        print(f'[red][-][/] セッションチェック中にエラーが発生しました: {e}')
+        return False
+
+
+def delete_session() -> None:
+    cookie_file = os.path.join('~', 'cache', 'atcder', 'session.json')
+    if os.path.exists(cookie_file):
+        os.remove(cookie_file)
